a0ea2a427301271e965c844614fd2626af234ec3
USAMI Kenta <tadsan@zonu.me>
2022-04-04 02:12:45 +0900

    Init repository

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace zonuexe\PHPerKaigi\Psr15;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+}
